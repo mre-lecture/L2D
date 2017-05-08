067d4eda942f7251ea271ca0fd9494e765191730
“Jens <jens.arendt@yahoo.de>
2017-05-08 15:03:01 +0200

    -added Scorescene, TextTutorialscene, BackToMenu Script and Score Script

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Score : MonoBehaviour {
+
+    public Text uiText;
+	// Use this for initialization
+	void Start () {
+        toString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    //calculate Score
+    int calculateScore()
+    {
+        int score = Random.Range(0, 10);
+        return score;
+    }
+
+    //print Score text on wall and prints Score
+    void toString()
+    {
+        uiText.text = "Du hast " + calculateScore() + " von 10 Punkten!";
+    }
+}
