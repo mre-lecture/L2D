e0fee17b640ab42827e53f3b248a75998362a553
mrpatpat <adrian.endrich@googlemail.com>
2017-05-22 23:18:08 +0200

    Adding Score Algorithm, Goal Marker. Adjusting speed and other settings

---
+++ Assets/L2D/L2DCar/Scripts/CalculateScore.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CalculateScore : MonoBehaviour
+{
+
+    public int score;
+    public int maxScore = 1000;
+
+    public Transform goal;
+
+    public TextMesh debugField;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        score = calculateScore();
+        debugField.text = "" + score;
+    }
+
+    private int calculateScore()
+    {
+        float distance = Vector3.Distance(gameObject.transform.position, goal.transform.position);
+        return (int)(1 / distance * maxScore);
+    }
+
+}
