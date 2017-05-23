f2e6a043c4c4ce507db5ff13bba769d4db2972e4
f4rg3n2 <jens.arendt@yahoo.de>
2017-05-23 17:19:56 +0200

    -added new level, animation and updated menuscene

--- Assets/L2D/L2DMain/Script/ToLevel.cs
+++ Assets/L2D/L2DMain/Script/ToLevel.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ToLevel : MonoBehaviour {
 
+    
     // Use this for initialization
     void Start () {
 		
@@ -14,7 +16,7 @@ public class ToLevel : MonoBehaviour {
 		
 	}
 
-    public void loadLevel(string levelName)
+    public void LoadLevel(string levelName)
     {
         Application.LoadLevel(levelName);
     }
