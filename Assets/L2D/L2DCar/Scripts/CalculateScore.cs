effe4272a89884423979c6a1123aa8c49db77071
“Jens <jens.arendt@yahoo.de>
2017-06-28 11:21:23 +0200

    timer in lvl after 30 secs score shows up and goes to menu

--- Assets/L2D/L2DCar/Scripts/CalculateScore.cs
+++ Assets/L2D/L2DCar/Scripts/CalculateScore.cs
@@ -1,31 +1,54 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 
 public class CalculateScore : MonoBehaviour
 {
-
-    public int score;
+    public Text debugField;
     public int maxScore = 1000;
-
     public Transform goal;
-
-    public TextMesh debugField;
+    static int score = 0;
+    
 
     void Start()
     {
+
     }
 
     void Update()
     {
-        score = calculateScore();
-        debugField.text = "" + score;
+        calculateScore();
+        returnToMenu();
     }
 
-    private int calculateScore()
+    public int calculateScore()
     {
         float distance = Vector3.Distance(gameObject.transform.position, goal.transform.position);
         return (int)(1 / distance * maxScore);
+        
+    }
+     public void returnToMenu()
+    {
+        int leftTime = (30 - (int)Time.time);
+        debugField.text = "Time left: " + leftTime + " Seconds";
+        if (leftTime <= 0.0)
+        {
+            debugField.text = "Your Score is: " + calculateScore();
+            if (leftTime <= -2)
+            {
+                Application.LoadLevel("MenuScene");
+            }
+        }
     }
 
+    public void loadMenu(float leftTime)
+    {
+        if (leftTime <= 0.0)
+        {
+            int score = calculateScore();
+            debugField.text = "Your Score is:" + score + "!";
+            Application.LoadLevel("MenuScene");
+        }
+    }
 }
